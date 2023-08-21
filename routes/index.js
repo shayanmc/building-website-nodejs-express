67f3f347fb5e2808f7f729b2a1684e8424040235
shayan <shayan.133331@gmail.com>
2023-08-21 20:08:30 +0330

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const speakersRoute = require('./speakers');
+const feedbackRoute = require('./feedback');
+
+const router = express.Router();
+
+module.exports = params => {
+  const { speakersService } = params;
+
+  router.get('/', async (req, res, next) => {
+    try {
+      const artwork = await speakersService.getAllArtwork();
+      const topSpeakers = await speakersService.getList();
+      return res.render('layout', {
+        pageTitle: 'Welcome',
+        template: 'index',
+        topSpeakers,
+        artwork,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+  router.use('/speakers', speakersRoute(params));
+  router.use('/feedback', feedbackRoute(params));
+
+  return router;
+};
